05a647d1a731979006c67b0bd880fdb238934f02
maheshchandra06 <maheshchandra06@users.noreply.github.com>
2020-06-23 15:01:36 -0500

    new project
    new project

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+{
+    id: '1',
+    name: 'Mahesh Ghatta',
+    username: 'mbabu',
+    email: 'mahesh.ghatta@gmail.com'
+},
+{
+    id: 2,
+    name: 'Pawan Koni',
+    username: 'pkoni',
+    email: 'pawan.koni@gmail.com'
+},
+{
+    id: 3,
+    name: 'Prabhas Raj',
+    username: 'mbabu',
+    email: 'prabhas.raj@gmail.com'
+},
+{
+    id: 4,
+    name: 'Ravi Teja',
+    username: 'rteja',
+    email: 'ravi.teja@gmail.com'
+},
+{
+    id: 5,
+    name: 'Tarak Nanda',
+    username: 'tnanda',
+    email: 'tarak.nanda@gmail.com'
+},
+{
+    id: 6,
+    name: 'Ram Charan',
+    username: 'ramc',
+    email: 'ram.charan@gmail.com'
+},
+]
